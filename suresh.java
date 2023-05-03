c78eb50ded13a817c01847febed7e6a7f8bf504d
suresh rawat <sureshrawatrj01@gmail.com>
2023-05-03 11:35:38 +0530

    java

---
+++ suresh.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String arr[]) 
+    {
+        int a, b;
+        Scanner ob=new Scanner(System.in);
+         
+        System.out.print("enter a no.");
+        a = ob.nextInt();
+
+        System.out.print("Enter 2nd no.");
+        b = ob.nextInt();
+        System.out.println("you entered" +a+ "And" + b);
+        }    
+    }
