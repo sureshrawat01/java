cca5cbf347d96cf9fb983ce8c28234bd5e95e50f
suresh rawat <sureshrawatrj01@gmail.com>
2023-05-20 09:04:25 +0530

    aray

--- divide.java
+++ divide.java
@@ -1,23 +1,58 @@
-import java.util.Scanner;
-class Demo
+class My
 {
-public static void main (String arr[])
- {
-  int i,n,count=0;
+    public static void main(String brr[])
+    {
+        int a[][];
+        a = new int[2][3];
 
-  Scanner ob=new Scanner(System.in);
-  System.out.println(" Enter 10 num");
-  
-  for(i=1;i<=10;i++)
-  {
-    n=ob.nextInt();
+        int b[][] = new int[4][5];
 
-  }
-  if(n==0)
-  {
-    count++;
-  {
-    System.out.println("count");
-  }
-}
+        int c[][] = new int[][]{{1,2,3}, {4,5,6}};
+        
+        int d[][] = {{1,2,3}, {4,5,6}};
+
+        for(int i=0;i<2; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                System.out.print(" " + a[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+
+        for(int i=0;i<4;i++)
+        {
+          for(int j=0;j<5;j++)
+          {
+            System.out.print(" " + b[i][j]);
+          }
+          System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        for(int i=0;i<2;i++)
+        {
+          for(int j=0;j<3;j++)
+          {
+            System.out.print(" " + c[i][j]);
+          }
+          System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        for(int i=0;i<2;i++)
+        {
+          for(int j=0;j<3;j++)
+          {
+            System.out.print(" " + d[i][j]);
+          }
+          System.out.println();
+        }
+
+    }
 }
